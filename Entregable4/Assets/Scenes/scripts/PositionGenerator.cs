2530a875ef33119d9c6d3f5771140b34cd0d87f0
Joan Arrom Sans <rakdos23yt@gmail.com>
2021-11-02 21:45:41 +0100

    Entregable4

---
+++ Entregable4/Assets/Scenes/scripts/PositionGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionGenerator : MonoBehaviour
+{
+    public Vector3 RandomPosition()
+    {
+        float RandomX = Random.Range(0, 8);
+        float RandomY = Random.Range(0, 8);
+        float RandomZ = Random.Range(0, 8);
+        return new Vector3(RandomX, RandomY, RandomZ);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            transform.position = RandomPosition();
+        }
+    }
+}
